b3035691fcb0cc7e859367ecb075e5c9a42b18cc
octeep <octeep@pm.me>
2022-01-30 19:07:40 +0000

    pass test 3

--- rango/urls.py
+++ rango/urls.py
@@ -17,6 +17,9 @@ from django.contrib import admin
 from django.urls import path, include
 from rango import views
 
+app_name = "rango"
+
 urlpatterns = [
-    path('', views.index, name="index"),
+    path("", views.index, name="index"),
+    path("about/", views.about, name="about"),
 ]
